a4b30bfff6351b1a301bbccb75e461637738b279
Fernando Quintanilla <df.quintanillah@gmail.com>
2023-04-25 22:45:34 -0600

    repferenci

--- sistema-votaciones/frmLogin.Designer.cs
+++ sistema-votaciones/frmLogin.Designer.cs
@@ -28,38 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.boton1 = new FontAwesome.Sharp.IconButton();
+            this.btnClose = new FontAwesome.Sharp.IconButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbUsuario = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // boton1
+            // btnClose
             // 
-            this.boton1.IconChar = FontAwesome.Sharp.IconChar.Heart;
-            this.boton1.IconColor = System.Drawing.Color.Black;
-            this.boton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.boton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.boton1.Location = new System.Drawing.Point(283, 167);
-            this.boton1.Name = "boton1";
-            this.boton1.Size = new System.Drawing.Size(129, 54);
-            this.boton1.TabIndex = 0;
-            this.boton1.Text = "Hola";
-            this.boton1.UseVisualStyleBackColor = true;
-            this.boton1.Click += new System.EventHandler(this.boton1_Click);
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(32)))), ((int)(((byte)(65)))));
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(32)))), ((int)(((byte)(65)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.IconChar = FontAwesome.Sharp.IconChar.Xmark;
+            this.btnClose.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClose.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnClose.IconSize = 24;
+            this.btnClose.Location = new System.Drawing.Point(745, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(53, 39);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(50)))), ((int)(((byte)(103)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(421, 129);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbUsuario.Location = new System.Drawing.Point(418, 102);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lbUsuario.TabIndex = 2;
+            this.lbUsuario.Text = "Usuario";
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(32)))), ((int)(((byte)(65)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.boton1);
+            this.Controls.Add(this.lbUsuario);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmLogin";
             this.Text = "LOGIN";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmLogin_MouseDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private FontAwesome.Sharp.IconButton boton1;
+        private FontAwesome.Sharp.IconButton btnClose;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbUsuario;
     }
 }
 
